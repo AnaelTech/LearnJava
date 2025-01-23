7bf450c7b1819a48df9058511a6f6945ffc619ed
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2025-01-23 13:39:51 +0100

    ✨ feat: First class Java

---
+++ MaPremiereClasse.java
@@ -0,0 +1,6 @@
+public class MaPremiereClasse {
+    public static void main(String[] args) {
+        // Affiche le message "Hello World !" dans la console
+        System.out.println("Hello World !");
+    }
+}
